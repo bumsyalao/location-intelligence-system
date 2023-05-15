dfb2fc9482029e5e63e51857fd9f0fc6320a63c8
Bunmi Alao <alaobunmi93@gmail.com>
2023-05-15 19:52:06 +0400

    inital project setup

---
+++ server.ts
@@ -0,0 +1,37 @@
+import express, { Request, Response } from 'express';
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = 3030;
+
+
+// MongoDB connection URL
+const mongoURL = process.env.DB_URL;
+
+// Connect to MongoDB
+mongoose.connect(mongoURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        console.log('Connected to MongoDB');
+    })
+    .catch((error: Error) => {
+        console.error('Error connecting to MongoDB:', error);
+    });
+
+
+
+console.log('Connected to MongoDB');
+
+// Set up routes and middleware here
+app.get("/", (req: Request, res: Response) => {
+    res.status(200).send("Hello World!");
+})
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
